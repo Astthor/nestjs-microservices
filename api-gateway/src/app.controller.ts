b03f8d8f0d3b7349dcd120802327d518e9981885
Astthor <a.bragason@gmail.com>
2023-06-08 23:21:33 +0200

    revert app.controler.ts

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,38 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import { Logger } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  const SERVICE_A_URL = process.env.SERVICE_A_URL || 'http://localhost:3001';
-  const SERVICE_B_URL = process.env.SERVICE_B_URL || 'http://localhost:3002';
-  app.use(
-    '/api/service-a',
-    createProxyMiddleware({
-      target: SERVICE_A_URL,
-      changeOrigin: true,
-      onProxyReq: (clientRequest, req) => {
-        Logger.log('Proxy for service A called');
-      },
-    }),
-  );
-  app.use(
-    '/api/service-b',
-    createProxyMiddleware({
-      target: SERVICE_B_URL,
-      changeOrigin: true,
-      onProxyReq: (clientRequest, req) => {
-        Logger.log('Proxy for service B called');
-      },
-    }),
-  );
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
-bootstrap();
