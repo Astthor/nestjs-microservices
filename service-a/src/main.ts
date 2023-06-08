4a52f3aecd5499b0bf2eb12777932a00ea6c3364
Astthor <a.bragason@gmail.com>
2023-06-08 23:53:45 +0200

    2: Service A setup

--- service-a/src/main.ts
+++ service-a/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const SERVICE_A_PORT = process.env.SERVICE_A_PORT || 'http://localhost:3001';
+  await app.listen(SERVICE_A_PORT);
+  Logger.log(`🚀 SERVICE_A is running on: http://localhost:${SERVICE_A_PORT}`);
 }
 bootstrap();
