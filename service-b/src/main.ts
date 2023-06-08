56aab2258296d2c107196dc604d5d1938ed416b6
Astthor <a.bragason@gmail.com>
2023-06-09 00:28:34 +0200

    3: add global prefix to service b

--- service-b/src/main.ts
+++ service-b/src/main.ts
@@ -5,7 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const SERVICE_B_PORT = process.env.SERVICE_B_PORT || 3002;
+  const globalPrefix = 'api/service-b';
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(SERVICE_B_PORT);
-  Logger.log(`🚀 SERVICE_B is running on: http://localhost:${SERVICE_B_PORT}`);
+  Logger.log(
+    `🚀 SERVICE_B is running on: http://localhost:${SERVICE_B_PORT}/${globalPrefix}`,
+  );
 }
 bootstrap();
