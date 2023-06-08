4f03fec768b7cc2748581035889efa9591a82736
Astthor <a.bragason@gmail.com>
2023-06-08 23:57:22 +0200

    2: Service a fix port default to port instead of url

--- service-a/src/main.ts
+++ service-a/src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const SERVICE_A_PORT = process.env.SERVICE_A_PORT || 'http://localhost:3001';
+  const SERVICE_A_PORT = process.env.SERVICE_A_PORT || 3001;
   await app.listen(SERVICE_A_PORT);
   Logger.log(`🚀 SERVICE_A is running on: http://localhost:${SERVICE_A_PORT}`);
 }
